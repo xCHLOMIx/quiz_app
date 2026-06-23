c4224900040dbaea10596ef525d9747cec96459e
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-23 23:23:56 +0200

    feat: edit reusable ProgressBar component

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -17,7 +17,7 @@ export function ProgressBar({ value, total, label }: ProgressBarProps) {
       </div>
       <div className="h-5 overflow-hidden border-2 border-black bg-white rounded-full">
         <div
-          className="h-full border-r-2 border-black bg-[#54d66a] transition-all duration-300 ease-out rounded-full"
+          className="h-full outline-2 bg-[#54d66a] transition-all duration-300 ease-out rounded-full"
           style={{ width: `${Math.min(100, Math.max(0, percentage))}%` }}
         />
       </div>
